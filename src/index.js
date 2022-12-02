a45281301ad0cedd9ad10d5d7b0e336695696437
guilhermehub12 <guilhermee1312@hotmail.com>
2022-12-01 23:47:53 -0300

    Create function to decimal (incomplete)

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-const binaryValue = document.querySelector("form input #binary-input");
+const binary = document.querySelector("form input #binary-input");
 const decimalInput = document.querySelector("form input #decimal-input");
+
+const binaryValue = binary.value;
+
+function tranformToDecimal(binaryValue) {
+
+}
+
+binaryValue.addEventListener('click', tranformToDecimal(binaryValue))
